49609cb121f7419d085c00caf06193e81321d270
Casimiro-Moliterni <moliternicasimiro@hotmail.it>
2025-02-07 14:15:58 +0100

    routing

--- core/router.class.php
+++ core/router.class.php
@@ -15,6 +15,36 @@ class Router
      */
     protected $url_dirty = '';
     /**
+     * Array con la lista delle rotte, callback associato , parametru e priorità
+     * @var array
+     */
+    protected $routes = array();
+    /**
+     * Array contenente la lista delle rotte prima di essere elaborate
+     * @var array
+     */
+    protected $routes_original = array();
+    /**
+     * stampa a video gli errori della tabella di routing. utilizzato per il debug
+     * @var  booleano
+     */
+    protected $show_errors = false;
+    /**
+     * funzione di callback delle rotte
+     * @var object
+     */
+    protected $callback = null;
+    /**
+     * rotta di default
+     * @var object
+     */
+    protected $default_route = null;
+    /**
+     * array di parametri passati alla callback
+     * @var array
+     */
+    protected $params = array();
+    /**
      * Costruttore
      * @method __construct
      * @param string $url
@@ -33,9 +63,6 @@ class Router
         $this->url_dirty = $url;
         //pulisco l'url rimuovendo il protocollo, il dominio e la directory base se esiste
         $this->url_clean = $this->_get_clean_url($this->url_dirty);
-
-        echo 'dirty url:'. $this->url_dirty.'<br>';
-        echo 'clean url:'. $this->url_clean.'<br>';
     }
 
    /**
@@ -73,15 +100,131 @@ class Router
              $original_route = $route;
              //Mi assicuro che la rotta finisca con lo /
              $route = rtrim($route,'/').'/';
-             // Catturo la rotta alfa numerica (0-9A-ZA-z_), formato: <:var_name>
-             $route = preg_replace('/\<\:(.*?)\>/','(?P<\1>[A-Za-z0-9\-\_]+)',$route);
-             // Catturo la rotta nel formato numeric0 (0-9), formato: <#var_name>
-             $route = preg_replace('/\<#(.*?)>/','(?P<\1>[0-9]+)',$route);
-            // Catturo la rotta nel formato wildcard, formato: <*var_name>
-            $route = preg_replace('/\<*(.*?)>/','(?P<\1>.+)',$route);
+           // Gestisco il caso di wildcard senza nome esplicito
+            $route = preg_replace('/\<\*\>/', '(?P<wildcard>.+)', $route);
             
+            // Catturo la rotta alfa numerica (0-9A-ZA-z_), formato: <:var_name>
+            $route = preg_replace('/\<\:([^>]+)\>/', '(?P<$1>[A-Za-z0-9\-\_]+)', $route);
+            
+            // Catturo la rotta nel formato numerico (0-9), formato: <#var_name>
+            $route = preg_replace('/\<#([^>]+)\>/', '(?P<$1>[0-9]+)', $route);
+            
+            // Catturo la rotta nel formato wildcard con nome, formato: <*var_name>
+            $route = preg_replace('/\<\*([^>]+)\>/', '(?P<$1>.+)', $route);
+
+             // Aggiunta di una costante di default se BASE_PATH non è definito
+             $base_path = defined('BASE_PATH') ? BASE_PATH : '';
+            // concludo la costruzione della regex
+            $route = '#^'.BASE_PATH.$route.'$#';
+
+            //controllo se la rotta esiste già nella tabella di routing
+            if(isset($this->routes[$priority][$route])){
+                //se la rotta è già registrata nella tabella di routing lancio un eccezione
+                if($this->show_errors){
+                    throw new Exception('L\'URI "'.htmlspecialchars($route).'" esiste già nella tabella di routing.');
+                }
+                return false;
+            }
+            
+            // aggiungo la rotta nella tabella di routing
+            $this->routes[$priority][$route] = $callback;
+            $this->routes_original[$priority][$route] = $original_route;
+
+     
    }
-            
+         
+   /**
+    * lancia la funzione relativa all'url corrente.
+    * @method run
+    * @return array
+    */
+     public function run(){
+        //flag per url matchat o no
+        $matched_route = false;
+        // ordino l'array con le rotte per priorità di esecuzione
+        ksort($this->routes);
+
+        // scorro le rotte in ordine di priorità
+        foreach($this->routes as $priority => $routes){
+           // Per ogni rotta mi ricavo la funzione di callback 
+           foreach($routes as $route => $callback){
+            // controllo se la rotta metcha l'url corrente
+            if(preg_match($route,$this->url_clean,$matches)){
+                // trovata una rotta corrispondente all'url 
+                $matched_route = true;
+                // Lista dei parametri passati alla callback
+                $params = array($this->url_clean);
+                // mi ricavo il nome dei parametri 
+                foreach($matches as $key => $match){
+                  $params[] = $match;
+                }
+                // imposto i parametri e la funzione di callback
+                $this->params = $params;
+                $this->callback = $callback;
+                // ritorno un vettore con tutti i dati della rotta
+                return array('callback'=> $callback,'params'=> $params, 'route' => $route, 'original_route' => $this->routes_original[$priority][$route]);
+            }
+           }
+        }
+        // se non è stata metchata nessuna rotta , imposto i valori di default dell'oggetto
+        if(!$matched_route && $this->default_route !== null){
+            $this->params = $callback;
+            $this->callback = $this->default_route;
+            $this->routes = false;
+            $this->routes_original = false;
+        }
+     }
+   /**
+    * chiama la funzione di callback appropriata e passa i parametri indicati forniti da Router->run()
+    * @method dispatch
+    * @return boolean
+    */
+     public function dispatch(){
+          if($this->callback === null || $this->params == null){
+            throw new Exception('nessuna callback o parametri trovati, eseguire $router->run prima di $router->dispatch');
+            return false;
+          }
+          call_user_func_array($this->callback, array($this->params));
+
+          return true;
+     }
+   /**
+    * Esegue il routing match e poi esegue la funziona di callback associata
+    * @method execute
+    */
+     public function execute(){
+        $this->run();
+        $this->dispatch();
+         }
+   /**
+    * Rotta di default. viene lanciata se non si trovano corrispondenze nell'url (es pagina 404)
+    * @method default_route
+    *@param object object $callaback
+    *@return object
+    */
+     public function default_route($callback){
+          $this->default_route = $callback;
+          return $this;
+     }
+   /**
+    * abilita la visualizzazione a video degli errori . per il debug
+    * @method show_errors
+    *@method show_errors
+    *@return object
+    */
+     public function show_errors(){
+       $this->show_errors = true;
+       return $this;
+     }
+   /**
+    * Disabilita la visualizzazione a video degli errori per il debug
+    * @method hide_error
+    *@return object
+    */
+     public function hide_errors(){
+        $this->show_errors = false;
+        return $this;
+     }
 
 }
 ?>
--- routing.php
+++ routing.php
@@ -3,10 +3,22 @@
 $Router->route('/',function(){
     echo'<h1>HOME</h1>';
 });
-// pagina error 404
-// $Router->default_route(function(){
-//     echo'<h1>error 404</h1>';
-// });
+//pagina chi siamo con parametro wildcard
+$Router->route('/chi-siamo/<*>',function($args){
+    echo'<h1>Chi siamo</h1>';
+});
+//pagina dove siamo con parametro numerico
+$Router->route('/dove-siamo/<#id>',function($args){
+    echo'<h1>Dovesiamo</h1>';
+});
+//pagina blog con parametro stringa
+$Router->route('/blog/<:slug>',function($args){
+    echo'<h1>Blog</h1>';
+});
+// pagina error 404 per url non trovato
+$Router->default_route(function(){
+    echo'<h1>error 404</h1>';
+});
 
-// $Router->execute();
+$Router->execute();
 ?>
